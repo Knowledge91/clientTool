8095f5526cb30b395647301f2efb39c0382d65ae
Knowledge <dirkhornung91@gmail.com>
2016-01-16 21:35:31 +0100

    Finished Angular functions

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 var App = angular.module('App', []);
 
-
-App.controller('mainCtrl', ['$scope', '$http' ,function($scope, $http) {
-    $http.get('clients.json').then(function(res){
-        $scope.clients = res.data;                
-     });
+App.controller('mainCtrl', ['$scope', '$http', function($scope, $http) {
+  $scope.name = '';
+  $scope.club = '';
+  $scope.telefon = '';
+    
+  $http.get('clients.json').then(function(res){
+      $scope.clients = res.data;                
+      $scope.clients[0].status = "bg-danger";
+      $scope.clients[0].comment = "no comments";
+      $scope.updateStatus = function(client, str){
+        client.status = str;
+      }
+   });
 }]);
